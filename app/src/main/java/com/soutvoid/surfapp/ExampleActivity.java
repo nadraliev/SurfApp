b761863256e87d6b94e00ee4f027e7e3feb9afe6
Andrew Nadraliev <andrew.nadraliev@hotmail.com>
2016-10-28 19:12:30 +0300

    добавлена ExampleActivity и кнопка перехода

---
+++ app/src/main/java/com/soutvoid/surfapp/ExampleActivity.java
@@ -0,0 +1,19 @@
+package com.soutvoid.surfapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andrew on 28-Oct-16.
+ */
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_example);
+    }
+}
